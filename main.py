04060313c05a9d56c2194b592b33f7bf8533086e
Romen <rw1017905@wcupa.edu>
2023-12-10 01:39:12 -0500

    deez

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import os
+from os.path import join,dirname
+from dotenv import load_dotenv
+
+import discord
+from discord.ext import commands
+
+dotenv_path=join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=">", intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+bot.run(os.environ.get("token"))
